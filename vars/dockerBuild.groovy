9608d577e9daf8272ac67e172e00f9ed4e38ebd4
shivakumar123441 <75583406+shivakumar123441@users.noreply.github.com>
2023-04-16 00:52:04 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -10,5 +10,5 @@ def call(String project, String hubUser) {
         sh "docker login -u '$USER' -p '$PASS'"
     }
     sh "docker image push ${hubUser}/${project}:${ImageTag}"
-    sh "docker image push ${hubUser}/${project}:latest"
+    //sh "docker image push ${hubUser}/${project}:latest"
 }
