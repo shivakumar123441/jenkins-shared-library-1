426dc24b82826aeb6b8ebb32c34af263b382c3b6
shivakumar123441 <75583406+shivakumar123441@users.noreply.github.com>
2023-04-16 00:52:35 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,7 @@
 def call(String project, String hubUser) {
     sh "docker image build -t ${hubUser}/${project} ."
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
-    sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
+    //sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
     withCredentials([usernamePassword(
             credentialsId: "docker_cred",
             usernameVariable: "USER",
